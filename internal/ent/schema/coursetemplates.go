b3d76562a2f728b0f3e415809a91d84edfc488d7
Leonardo Ramírez <juanlov4321@gmail.com>
2026-09-25 12:26:22 -0500

    Add course category and template schemas
    Introduce reusable tenant and timestamp mixins with generated Ent
    CRUD and query support for the new entities

---
+++ internal/ent/schema/coursetemplates.go
@@ -0,0 +1,33 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+)
+
+// CourseTemplates holds the schema definition for the CourseTemplates entity.
+type CourseTemplates struct {
+	ent.Schema
+}
+
+// Fields of the CourseTemplates.
+func (CourseTemplates) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("id").StorageKey("id").Immutable(),
+		field.String("code").Unique().NotEmpty().MaxLen(200),
+		field.String("title").NotEmpty().MaxLen(200),
+		field.Text("description").Default("").Optional().NotEmpty(),
+	}
+}
+
+// Edges of the CourseTemplates.
+func (CourseTemplates) Edges() []ent.Edge {
+	return nil
+}
+
+func (CourseTemplates) Mixin() []ent.Mixin {
+	return []ent.Mixin{
+		TenantMixin{},
+		TimeMixin{},
+	}
+}
---
+++ internal/ent/schema/timemixin.go
@@ -0,0 +1,26 @@
+package schema
+
+import (
+	"time"
+
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+)
+
+// TimeMixing holds the schema definition for the TimeMixing entity.
+type TimeMixin struct {
+	ent.Schema
+}
+
+// Fields of the TimeMixing.
+func (TimeMixin) Fields() []ent.Field {
+	return []ent.Field{
+		field.Time("created_at").Default(time.Now()).Immutable(),
+		field.Time("updated_at").Default(time.Now()).UpdateDefault(time.Now),
+	}
+}
+
+// Edges of the TimeMixing.
+func (TimeMixin) Edges() []ent.Edge {
+	return nil
+}
